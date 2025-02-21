89b4d961872784a418d171f4bf93362c9e70cabd
lllendexll <lllendexll@outlook.com>
2025-02-20 22:20:59 -0800

    first commit

---
+++ BlazorStudents/Models/Constants.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlazorStudents.Models;
+
+public class Constants
+{
+    //public const string BASE_URL = "https://studentfunctionmze.azurewebsites.net/";
+    //public const string BASE_URL = "http://localhost:7071/";
+    public const string BASE_URL = "/";
+
+}
